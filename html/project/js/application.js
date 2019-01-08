a118572b181789675966cebb5da534d5b736e7dc
Gian Pumayalla <g.pumayalla@hotmail.com>
2019-01-08 14:16:03 +0100

    init animation

--- html/project/js/application.js
+++ html/project/js/application.js
@@ -2,6 +2,7 @@ let activatorNav = document.querySelector('#activator-nav');
 let burgherNav = document.querySelector('#burgher-nav');
 
 activatorNav.addEventListener('click',function(){
+    console.log("click");
     burgherNav.classList.toggle('transition');
 })
 
