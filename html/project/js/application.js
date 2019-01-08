709b5d27369d1ba3945b31e2bb7141928187f35a
Gian Pumayalla <g.pumayalla@hotmail.com>
2019-01-08 18:01:36 +0100

    creato script

--- html/project/js/application.js
+++ html/project/js/application.js
@@ -1,11 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
+
+   //nav aside
    let activatorNav = document.querySelector('#activator-nav');
    let burgherNav = document.querySelector('#burgher-nav');
 
    let body = document.body;
 
    activatorNav.addEventListener('click', function () {
-      
+
       if (burgherNav.classList.contains('nav-start')) {
          burgherNav.classList.add('nav-end');
          if (burgherNav.classList.contains('nav-start')) {
@@ -24,5 +26,32 @@ document.addEventListener("DOMContentLoaded", function () {
 
       body.classList.toggle('lock');
    })
+
+
+   //Checkbox
+   let inputName = document.querySelector('.input-name');
+   let inputEmail = document.querySelector('.input-email');
+   let pName = document.querySelector('.p-name');
+   let pEmail = document.querySelector('.p-email');
+
+
+   inputName.addEventListener('click',function(){
+      pName.classList.add('up');
+   })
+   inputName.addEventListener('mouseout',function(){
+      pName.classList.remove('up');
+      if(inputName.value.length > 0 ){
+         pName.classList.add('up');
+      }
+   })
+
+   inputEmail.addEventListener('mouseout',function(){
+      pEmail.classList.remove('up');
+      if(inputEmail.value.length > 0 ){
+         pEmail.classList.add('up');
+      }
+   })
+
+   
 
 })
